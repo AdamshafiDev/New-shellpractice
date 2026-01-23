c4846fa3535a4ba9773b235ec821b8635a14d047
adamshafi <adamshafi0001.com>
2026-01-23 09:13:04 +0530

    Hello

---
+++ 2-instance.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0058396fcd7f24096"
+INSTANCES=("mongodb" "redis" "mysql" "frontend")
+ZONE_ID="Z057398224FSX4EUYNDH2"
+DOMAIN_NAME="adamshafi.shop"
+
+for INSTANCES in ${INSTANCES[@]}
+do
+
+if [ ]
+
+done
