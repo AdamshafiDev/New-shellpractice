630b6cfcd23a33b6b08f6f5fc65eb6fab814e911
adamshafi <adamshafi0001.com>
2026-01-23 09:43:06 +0530

    Hello

--- 4-Testdata.sh
+++ 4-Testdata.sh
@@ -11,51 +11,20 @@ echo "the script started time:$(date)"
 if [ $USERID -ne 0 ]
 then
   echo "ERROR:please run the script with root access"
-     exit 1
+  exit 1
  else
   echo "the script is running with root access"
   fi
 
-VALIDATE()
-{ 
-    if [ $1 -eq 0 ]
-     then
-     echo "Installling $2...SUCCESSS"
-     else
-     echo "installing $2 .....Failure"
-     exit 1
-     fi
-}
+# VALIDATE()
+# { 
+#     if [ $1 -eq 0 ]
+#      then
+#      echo "Installling $2...SUCCESSS"
+#      else
+#      echo "installing $2 .....Failure"
+#      exit 1
+#      fi
+# }
 
-dnf list installed nginx
 
-if [ $? - ne 0 ]
- then
- echo "nginx is not install.....going to install now"
- dnf install nginx -y
- VALIDATE $1 "nginx"
- else 
-  echo "nginx is already installed ...Nothing to do"
-  fi
-
-dnf list installed python3
-
-if [ $? - ne 0 ]
- then
- echo "python3 is not install.....going to install now"
- dnf install python3 -y
- VALIDATE $1 "python3"
- else 
-  echo "python3 is already installed ...Nothing to do"
-  fi
-
-dnf list installed mysql
-
-if [ $1 - ne 0 ]
- then
- echo "mysql is not install.....going to install now"
- dnf install mysql -y
- VALIDATE $? "mysql"
- else 
-  echo "mysql is already installed ...Nothing to do"
-  fi
