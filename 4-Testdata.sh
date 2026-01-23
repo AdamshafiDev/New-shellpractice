f7a542352a318aee0b3c272836f9c7ed01e58983
adamshafi <adamshafi0001.com>
2026-01-23 10:36:56 +0530

    Hello

--- 4-Testdata.sh
+++ 4-Testdata.sh
@@ -7,22 +7,28 @@ Y="\e[33m"
 N="\e[0m"
 #PACK=("nginx" "mysql" "python3" "httpd")
 
-echo "the script started time:$(date)"
+LOG_FOLDER=/var/log/shellscript.logs
+SCRIPT_NAME=echo $0 | cut -d "." -f1
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER
+
+echo  "the script started time:$(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-  echo "ERROR:please run the script with root access"
+  echo -e "$R ERROR:please run the script with root access" | tee -a $LOG_FILE
   exit 1
  else
-  echo "the script is running with root access"
+  echo -e "$Y the script is running with root access" | tee -a $LOG_FILE
   fi
 
 VALIDATE(){ 
     if [ $1 -eq 0 ]
      then
-     echo "Installling $2...SUCCESSS"
+     echo "Installling $2...SUCCESSS" | tee -a $LOG_FILE
      else
-     echo "installing $2 .....Failure"
+     echo "installing $2 .....Failure" | tee -a $LOG_FILE
      exit 1
      fi
 }
@@ -34,11 +40,11 @@ VALIDATE(){
     dnf list installed $pack
      if [ $? -ne 0 ]
     then
-        echo "$pack is not install....Going to install now"
+        echo "$pack is not install....Going to install now" | tee -a $LOG_FILE
         dnf install $pack -y
         VALIDATE $? "$pack"
     else
-        echo "$pack is already installed ....nothing to do"
+        echo -e  "$pack is already installed $G ....nothing to do" | tee -a $LOG_FILE
      fi
 
    done 
