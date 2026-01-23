07f0f653aff429ea8f0cca2e9adafc8ac03607a4
adamshafi <adamshafi0001.com>
2026-01-23 09:19:44 +0530

    Hello

---
+++ 4-Testdata.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "the script started time:$(date)"
+
+if [ $USERID -ne 0 ]
+then
+  echo "ERROR:please run the script with root access"
+     exit 1
+ else
+  echo "the script is running with root access"
+  fi
+
+VALIDATE()
+{ 
+    if [ $1 -eq 0 ]
+     then
+     echo "Installling $2...SUCCESSS"
+     else
+     echo "installing $2 .....Failure"
+     exit 1
+     fi
+}
+
+dnf list  installed nginx
+
+if [ $? - ne 0 ]
+ then
+ echo "nginx is not install.....going to install now"
+ dnf install nginx -y
+ VALIDATE $? "nginx"
+ else 
+  echo "nginx is already installed ...Nothing to do"
+  fi
+
