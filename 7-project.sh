d8fa062de1d1c3156b581aeb6604ed9691464989
adamshafi <adamshafi0001.com>
2026-01-25 09:16:07 +0530

    Hello

--- 7-project.sh
+++ 7-project.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-#TIME_STAMP=$(date)
+TIME_STAMP=$(date)
+Pack=("Mysql" "nginx" "nodejs")
  echo "the script started date::$TIME_STAMP"
 
 if [ $USERID -ne 0 ]
@@ -11,3 +12,23 @@ then
 else
    echo "the script is running with root access"
 fi
+   
+   VALIDATE(){
+
+        if [ $1 -eq 0 ]
+        then
+          echo "Installing package:$2.....successs"
+        else
+          echo "Installing package:$2.....Failure" 
+          exit 1
+   }
+
+   dnf  list installed mysql
+   
+     for Pack  in ( $Pack[@])
+     do
+       echo "$pack is not install going to install"
+       dnf install $pack -y
+       Validate $? "mysql"
+       
+       done
