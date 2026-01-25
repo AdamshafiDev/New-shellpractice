9c0c78936bd3d22183d8a7f92eb61cea0b91be34
adamshafi <adamshafi0001.com>
2026-01-25 11:12:56 +0530

    Hello

--- 7-project.sh
+++ 7-project.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIME_STAMP=$(date)
 #PACKAGE=("mysql" "nginx" "nodejs")
-LOG_FOLDER="/var/log/shellscript-log"
+LOG_FOLDER=$("/var/log/shellscript-log")
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 PACKAGE=("mysql" "nginx" "nodejs" "python3")
