284f652712ef60b93427ae65e5ad3b45099dc31b
adamshafi <adamshafi0001.com>
2026-01-25 12:41:05 +0530

    Hello

--- 8-project2.sh
+++ 8-project2.sh
@@ -9,26 +9,26 @@ Script_name=$(echo $0 | cut -d "." -f1)
 Log_file="$LOG_FOLDER/$Script_name.log"
 pack=("nginx" "mysql" "nodejs")
 
- mkdir -p "$LOG_FOLDER"
+ mkdir -p $LOG_FOLDER
 
  TIME_STAMP=$(date)
-  echo "the script start time$TIME_STAMP" | tee -a "$Log_file"
+  echo "the script start time$TIME_STAMP" | tee -a $Log_file
 
 if [ $USERID -ne 0 ]
 then
-  echo -e "$$R ERROR: please run the script with  root access" | tee -a "$Log_file"
+  echo -e "$$R ERROR: please run the script with  root access" | tee -a $Log_file
   exit 1
    else 
-   echo -e "$Y the script running with root access" | tee -a "$Log_file"
+   echo -e "$Y the script running with root access" | tee -a $Log_file
  fi
 
    VALIDATE()
    {
     if [ $1 -eq 0 ]
     then
-    echo -e "$Y Installing $2.....$G success" | tee -a "$Log_file"
+    echo -e "$Y Installing $2.....$G success" | tee -a $Log_file
      else 
-     echo -e "$Y Installing $2......$R failure"| tee -a "$Log_file"
+     echo -e "$Y Installing $2......$R failure"| tee -a $Log_file
      exit 1
      fi
    }
@@ -39,11 +39,11 @@ then
               if [ $? -ne 0 ]
                then
 
-               echo "$cover is not insall...going to inatall now" | tee -a "$Log_file"
+               echo "$cover is not insall...going to inatall now" | tee -a $Log_file
                 dnf install $cover -y &>>$Log_file
                 VALIDATE $? "$cover"
                 else
-            echo " $cover is already installed nothing to do" | tee -a "$Log_file"
+            echo " $cover is already installed nothing to do" | tee -a $Log_file
                 fi
 
     done
