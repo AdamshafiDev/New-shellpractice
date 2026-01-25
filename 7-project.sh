de79cda587c7b723c3e0af57eef8d99d8c665f74
adamshafi <adamshafi0001.com>
2026-01-25 09:02:21 +0530

    Hello

---
+++ 7-project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIME_STAMP=$(date)
+ echo "the script started date::$TIME_STAMP"
+
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR:plese run the script with root access"
+   exit 1
+else
+   echo "the script is running with root access"
+fi
