d2e8a385950d79d3b36afed69b7858ed64e2e286
adamshafi <adamshafi0001.com>
2026-01-25 11:32:12 +0530

    Hello

--- 8-project2.sh
+++ 8-project2.sh
@@ -5,8 +5,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOG_FOLDER="/var/log/shelllogs.sh"
+Script_name=($0)
+Log_file="$LOG_FOLDER/$Script_name"
+
  TIME_STAMP=$(date)
-  echo "the script start time$TIME_STAMP"
+  echo "the script start time$TIME_STAMP" | tee -a $Log_file
 
 if [ $USERID -ne 0 ]
 then
