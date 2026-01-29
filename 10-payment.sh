2cd3e548699db9862610473ac4df9c89dd353f73
adamshafi <adamshafi0001.com>
2026-01-29 11:03:30 +0530

    Hello

--- 10-payment.sh
+++ 10-payment.sh
@@ -1,78 +1,83 @@
 #!/bin/bash
+
 USERID=$(id -u)
-R="\e[31m"s
+
+R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOG_FOLDER="/var/log/shelllogs.logs"
-Script_name=$(echo $0 | cut -d "." -f1)
-Log_file="$LOG_FOLDER/$Script_name.log"
-SCRIPT_DIR=$PWD
 
- mkdir -p $LOG_FOLDER
+LOG_FOLDER="/var/log/shelllogs"
+SCRIPT_NAME=$(basename "$0" .sh)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
- TIME_STAMP=$(date)
-  echo "the script start time$TIME_STAMP" | tee -a $Log_file
+mkdir -p "$LOG_FOLDER"
 
-if [ $USERID -ne 0 ]
-then
-  echo -e "$$R ERROR: please run the script with  root access" | tee -a $Log_file
+TIME_STAMP=$(date)
+echo "Script started at: $TIME_STAMP" | tee -a "$LOG_FILE"
+
+# Root check
+if [ "$USERID" -ne 0 ]; then
+  echo -e "$R ERROR: please run the script with root access $N" | tee -a "$LOG_FILE"
   exit 1
-   else 
-   echo -e "$Y the script running with root access" | tee -a $Log_file
- fi
+else
+  echo -e "$Y Script running with root access $N" | tee -a "$LOG_FILE"
+fi
 
-   VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-    echo -e "$2 is....$G success $N" | tee -a $Log_file
-     else 
-     echo -e "$2 is.....$R failure $N"| tee -a $Log_file
-     exit 1
-     fi
-   }
-
-
- dnf install python3 gcc python3-devel -y &>>$Log_file
- VALIDATE $? "Installing pyrhon3"
-
-
-
-   id roboshop
-
-  if [ $? -ne 0 ]
-   then
-        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$Log_file
-        VALIDATE $? "systemuser cerated"
+# Validate function
+VALIDATE() {
+  if [ "$1" -eq 0 ]; then
+    echo -e "$2 is... $G SUCCESS $N" | tee -a "$LOG_FILE"
   else
-     echo -e "roboshop user already created....$Y skipping"
-     fi
+    echo -e "$2 is... $R FAILURE $N" | tee -a "$LOG_FILE"
+    exit 1
+  fi
+}
 
+# Install packages
+dnf install python3 gcc python3-devel -y &>>"$LOG_FILE"
+VALIDATE $? "Installing Python3 packages"
 
-mkdir -p /app  &>>$Log_file
-VALIDATE $? "Directory cerated"
+# Create roboshop user if not exists
+if ! id roboshop &>/dev/null; then
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>"$LOG_FILE"
+  VALIDATE $? "Roboshop user creation"
+else
+  echo -e "$Y Roboshop user already exists... skipping $N" | tee -a "$LOG_FILE"
+fi
 
-curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>>$Log_file
-VALIDATE $? "downloading payment data"
- rm -rf /app/*
-cd /app 
+# Create app directory
+mkdir -p /app &>>"$LOG_FILE"
+VALIDATE $? "Creating /app directory"
 
-unzip /tmp/payment.zip &>>$Log_file
-VALIDATE $? "unzipping foldercatalouge"
+# Download application
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>"$LOG_FILE"
+VALIDATE $? "Downloading payment artifact"
 
+cd /app || exit 1
+rm -rf /app/*
 
- pip3 install -r requirements.txt &>>$Log_file
- VALIDATE $? "Installing dependicies"
+unzip /tmp/payment.zip &>>"$LOG_FILE"
+VALIDATE $? "Unzipping payment application"
 
- cp payment.service /etc/systemd/system/payment.service &>>$Log_file
- VALIDATE $? "copying the "
+# Install dependencies
+pip3 install -r requirements.txt &>>"$LOG_FILE"
+VALIDATE $? "Installing Python dependencies"
 
+# Set ownership
+chown -R roboshop:roboshop /app
 
-systemctl daemon-reload &>>$Log_file
-VALIDATE $? "Deamon reloaded"
+# Setup service
+cp payment.service /etc/systemd/system/payment.service &>>"$LOG_FILE"
+VALIDATE $? "Copying payment service file"
 
-systemctl enable payment  &>>$Log_file
-VALIDATE $? "enableing payments"
+systemctl daemon-reload &>>"$LOG_FILE"
+VALIDATE $? "Systemd daemon reload"
 
-systemctl start payment &>>$Log_file
-VALIDATE $? "starting payments"
+systemctl enable payment &>>"$LOG_FILE"
+VALIDATE $? "Enabling payment service"
+
+systemctl start payment &>>"$LOG_FILE"
+VALIDATE $? "Starting payment service"
+
+echo -e "$G Payment service deployed successfully $N" | tee -a "$LOG_FILE"
