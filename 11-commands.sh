ce22edbe7fd1bcb125aa46ee1692bc26ad6a0606
adamshafi <adamshafi0001.com>
2026-07-20 15:49:22 +0530

    hello add

--- 11-commands.sh
+++ 11-commands.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
- echo -n  "enter the pin number:"
+#  echo -n  "enter the pin number:"
   
-  read  PIN
+#   read -s  PIN
 
-  echo "the number is:"$PIN
+#   echo "the number is:"$PIN
+
+
+
+
+echo -n "Enter PIN: "
+PIN=""
+
+while IFS= read -rsn1 key
+do
+    # Enter key pressed
+    if [[ $key == "" ]]; then
+        break
+    fi
+
+    PIN+="$key"
+    echo -n "*"
+done
+
+echo
+echo "Entered PIN: $PIN"
